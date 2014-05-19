972f113686512a4645968908fd989c912b754fd8
menno de boer <run_jongejan@hotmail.com>
2014-05-19 14:49:42 +0200

    ability01

---
+++ Assets/CannonBullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBullet : BulletController {
+	private float bombRadius = 7.5f;
+	private float bombDamage = 700f;
+	public GameObject explosion;
+	void Start()
+	{
+		speed = 10f;
+	}
+	protected override void OnTriggerEnter2D(Collider2D other) {
+		if(other.transform.tag == "Enemy") 
+		{
+			Explode();
+		}
+	}
+	private void Explode()
+	{
+		Collider2D[] colliders = Physics2D.OverlapCircleAll (transform.position, bombRadius);
+		foreach(Collider2D col in colliders)
+		{
+			if(col.transform.tag == "Enemy") 
+			{
+				col.GetComponent<EnemyController>().LoseHealth(bombDamage, false);
+			}
+		}
+		Vector3 pos = this.transform.position;
+		pos.z = -0.4f;
+		Instantiate (explosion, pos, this.transform.rotation);
+		Destroy (this.gameObject);
+	}
+}
